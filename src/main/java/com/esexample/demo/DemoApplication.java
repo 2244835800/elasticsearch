98995d19523b996ee5e90e1039852033b42f6d03
hepo <2244835800@qq.com>
2018-11-16 16:48:32 +0800

    MOD:添加删除和更新操作

--- src/main/java/com/esexample/demo/DemoApplication.java
+++ src/main/java/com/esexample/demo/DemoApplication.java
@@ -1,7 +1,10 @@
 package com.esexample.demo;
 
+import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.concurrent.ExecutionException;
 
 @SpringBootApplication
 @RestController
@@ -63,6 +67,52 @@ public class DemoApplication {
         }
     }
 
+    @DeleteMapping("/delete/people/man")
+    @ResponseBody
+    public ResponseEntity delete(@RequestParam(name = "id", defaultValue = "") String id) {
+        DeleteResponse result = this.client.prepareDelete("people", "man", id).get();
+        if (id.isEmpty()) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity(result.getResult().toString(), HttpStatus.OK);
+    }
+    @PutMapping("/update/people/man")
+    @ResponseBody
+    public ResponseEntity update(
+            @RequestParam(name = "id") String id,
+            @RequestParam(name = "name",required = false) String name,
+            @RequestParam(name = "age",required = false ) String age,
+            @RequestParam(name = "date",required = false)
+//                    @DateTimeFormat(pattern = "yyyy-MM-dd")
+                    String date,
+            @RequestParam(name = "country",required = false) String country) {
+
+        UpdateRequest update=new UpdateRequest("people","man",id);
+        try {
+            XContentBuilder builder = XContentFactory.jsonBuilder()
+                    .startObject();
+            if(name!=null)
+                builder.field("name", name);
+            if(age!=null)
+                builder.field("age", age);
+            if(date!=null)
+                builder.field("date", date);
+            if(country!=null)
+                builder.field("country", country);
+            builder.endObject();
+            update.doc(builder);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        try {
+            UpdateResponse result= this.client.update(update).get();
+            return new ResponseEntity(result.getResult().toString(), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
